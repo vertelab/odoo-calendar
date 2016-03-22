554a9f8db5dab16a9cc75a9a7cc6833dac41c259
NSvensson <niklas_svensson87@hotmail.com>
2016-03-22 11:26:36 +0100

    calendar_ics:Added a module that creates the swedish holiday events to the calendar and the swedish taxation dates

---
+++ l10n_se_calendar/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_se_calendar",
+
+    'summary': """
+        Adds swedish holidays and dates of interest to your calendar.""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Vertel AB",
+    'website': "http://www.vertel.se",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['calendar_ics'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'res_partner_data.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #~ 'demo.xml',
+    ],
+}
---
+++ l10n_se_calendar/models.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
