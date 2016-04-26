e36d5e1af5564dc72feafe443e257fccda56e439
HaojunZou <apollo_zhj@msn.com>
2016-04-26 16:51:49 +0200

    calendar_kanban: _group_by_full method implemented

--- calendar_kanban/calendar_kanban.py
+++ calendar_kanban/calendar_kanban.py
@@ -21,7 +21,7 @@
 from openerp import models, fields, api, _
 import time
 import datetime
-from datetime import date
+from datetime import date, timedelta
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -29,8 +29,40 @@ _logger = logging.getLogger(__name__)
 class calendar_event(models.Model):
     _inherit = 'calendar.event'
 
+    # return a list of next six weeks from now
+    def next_six_weeks():
+        now = datetime.date.today()
+        return[
+            str(now.isocalendar()[0]) + '-W' + str(now.isocalendar()[1]),
+            str((now + datetime.timedelta(weeks=1)).isocalendar()[0]) + '-W' + str((now + datetime.timedelta(weeks=1)).isocalendar()[1]),
+            str((now + datetime.timedelta(weeks=2)).isocalendar()[0]) + '-W' + str((now + datetime.timedelta(weeks=2)).isocalendar()[1]),
+            str((now + datetime.timedelta(weeks=3)).isocalendar()[0]) + '-W' + str((now + datetime.timedelta(weeks=3)).isocalendar()[1]),
+            str((now + datetime.timedelta(weeks=4)).isocalendar()[0]) + '-W' + str((now + datetime.timedelta(weeks=4)).isocalendar()[1]),
+            str((now + datetime.timedelta(weeks=5)).isocalendar()[0]) + '-W' + str((now + datetime.timedelta(weeks=5)).isocalendar()[1]),
+        ]
+
+    # select attribut to week_number field
+    WEEKS = [
+        (next_six_weeks()[0], next_six_weeks()[0]),
+        (next_six_weeks()[1], next_six_weeks()[1]),
+        (next_six_weeks()[2], next_six_weeks()[2]),
+        (next_six_weeks()[3], next_six_weeks()[3]),
+        (next_six_weeks()[4], next_six_weeks()[4]),
+        (next_six_weeks()[5], next_six_weeks()[5]),
+    ]
+
+    # which week shows folded by default
+    FOLDED_WEEK = [
+        next_six_weeks()[0],
+        next_six_weeks()[1],
+        next_six_weeks()[2],
+        next_six_weeks()[3],
+        next_six_weeks()[4],
+        next_six_weeks()[5],
+    ]
+
     color = fields.Integer(string='Color Index')
-    week_number = fields.Char(string='Week number', inverse='set_week_number', readonly=True, store=True)
+    week_number = fields.Selection(WEEKS, string='Week number', inverse='set_week_number', readonly=True, store=True)
 
     @api.one
     def get_week_number(self):
@@ -59,3 +91,33 @@ class calendar_event(models.Model):
                 'start_datetime': fields.Date.to_string(datetime.datetime.strptime(self.week_number + '-' + week_day, '%Y-W%W-%w')) + ' ' + meeting_start,
                 'stop_datetime': fields.Date.to_string(datetime.datetime.strptime(self.week_number + '-' + week_day, '%Y-W%W-%w')) + ' ' + meeting_stop,
             })
+
+    @api.model
+    def weeks_list(self, present_ids, domain, **kwargs):
+        folded = {key: (key in self.FOLDED_WEEK) for key, _ in self.WEEKS}
+        return self.WEEKS[:], folded
+
+    # group list for kanban view
+    _group_by_full = {
+        'week_number': weeks_list,
+    }
+
+    def _read_group_fill_results(self, cr, uid, domain, groupby,
+                                 remaining_groupbys, aggregated_fields,
+                                 count_field, read_group_result,
+                                 read_group_order=None, context=None):
+        """
+        The method seems to support grouping using m2o fields only,
+        while we want to group by a week_number field.
+        Hence the code below - it replaces simple week_number values
+        with (value, name) tuples.
+        """
+        if groupby == 'week_number':
+            WEEK_DICT = dict(self.WEEKS)
+            for result in read_group_result:
+                week = result['week_number']
+                result['week_number'] = (week, WEEK_DICT.get(week))
+        return super(calendar_event, self)._read_group_fill_results(
+            cr, uid, domain, groupby, remaining_groupbys, aggregated_fields,
+            count_field, read_group_result, read_group_order, context
+        )
