3ab1ab717634f38a8df055b524ac15bda6e53635
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-29 11:43:58 +0200

    Update __manifest__.py

--- calendar_ics/__manifest__.py
+++ calendar_ics/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Calendar ics-urls',
+    'name': 'Calendar: ics-urls',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Subscription on calendar.ics-urls',
