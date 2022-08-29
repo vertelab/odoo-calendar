7cedb983834675a3cd01f2fed0053764076bbecd
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-29 14:11:59 +0200

    Update __manifest__.py

--- one_page_website_calendar/__manifest__.py
+++ one_page_website_calendar/__manifest__.py
@@ -1,16 +1,43 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'One Page Website Calendar',
-    'version': '1.0',
-    'category': 'Marketing/Online Appointment',
-    'sequence': 131,
+    'name': 'Calendar: One Page Website Calendar',
+    'version': '14.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Schedule bookings with clients',
-    'website': 'https://vertel.se/apps/one_page_website_calendar',
+    'category': 'Productivity',
     'description': """
         Allow clients to Schedule Bookings through your Website
         -------------------------------------------------------
     """,
+    #'sequence': '1',
+    'sequence': '131',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-calendar/one_page_website_calendar',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-calendar',
     'depends': ['calendar_sms', 'hr', 'website', 'website_calendar_ce'],
     'data': [
         'views/assets.xml',
@@ -23,3 +50,4 @@
     'application': True,
     'auto_install': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
