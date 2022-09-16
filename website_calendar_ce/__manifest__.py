9611853d8ea4d033d9947478daf9da2200038e57
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-16 14:31:46 +0200

    Update __manifest__.py

--- website_calendar_ce/__manifest__.py
+++ website_calendar_ce/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Schedule bookings with clients',
-    'category': 'Productivity',
+    'category': 'Calendar',
     'description': """
     Allow clients to Schedule Bookings through your Website
     -------------------------------------------------------
