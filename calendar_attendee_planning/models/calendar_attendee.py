4e1a55c8e50e03bd89f70855c48e0155a030804a
Dmitri Iselund <dmitri.iselund@vertel.se>
2022-10-26 07:07:52 +0000

    push to utv14

--- calendar_attendee_planning/models/calendar_attendee.py
+++ calendar_attendee_planning/models/calendar_attendee.py
@@ -9,12 +9,14 @@ class CalendarAttendee(models.Model):
     _inherit = "calendar.attendee"
     _rec_name = 'event_id'
 
-    def _compute_week_number(self):
-        # self.event_week = datetime.date(self.event_date_start).isocalendar()[1]
-        _logger.warning(datetime.date(self.event_date_start).isocalendar()[1])
-        _logger.warning(self.event_date_start)
-
     user_id = fields.Many2one(related='event_id.user_id', store=True)
     event_date_start = fields.Datetime(related='event_id.start', store=True)
     event_date_end = fields.Datetime(related='event_id.stop')
-    event_week = fields.Integer(compute='_compute_week_number', store=True)
+    event_week = fields.Char(compute='_compute_week_number', store=True)
+
+    @api.depends('event_date_start')
+    def _compute_week_number(self):
+        for rec in self:
+            _logger.warning(f"{rec.event_date_start}")
+
+            rec.event_week = datetime.date(rec.event_date_start).isocalendar()[1]
