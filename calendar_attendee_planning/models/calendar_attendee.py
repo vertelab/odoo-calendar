20530637ddce486b10f62e29c434dac784cb56fb
Dmitri Iselund <dmitri.iselund@vertel.se>
2022-11-30 07:19:21 +0000

    interaction with resource.calendar, not yet completely done

--- calendar_attendee_planning/models/calendar_attendee.py
+++ calendar_attendee_planning/models/calendar_attendee.py
@@ -2,7 +2,8 @@
 from odoo import models, fields, api
 import logging
 import datetime
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+import pytz
 import numpy as np
 
 _logger = logging.getLogger(__name__)
@@ -74,39 +75,109 @@ class CalendarAttendee(models.Model):
         return employees
         
     def write(self, vals):
-        for rec in self:
-            res = super(CalendarAttendee, rec).write(vals)
-            if vals.get('partner_id',) or vals.get('event_date_start',):
-                partner = self.env['res.partner'].browse(self.partner_id.id)
-                #fix error that triggers when partner does not have employee id
-                employee_id = partner.user_ids[0].employee_id[0].id
 
-                leave_periods = self.env['hr.leave'].search([('employee_id', '=', employee_id)]).ids
+        res = super(CalendarAttendee, self).write(vals)
+        if vals.get('partner_id',) or vals.get('event_date_start',):
+            write_state = 'accepted'
 
-                for leave_id in leave_periods:
-                    leave = self.env['hr.leave'].browse(leave_id)
-                    # try:
-                    if leave.date_from <= self.event_date_end and self.event_date_start <= leave.date_to:
-                        self.write({'state': 'declined'})
-                        break
-                    else:
-                        self.write({'state': 'accepted'})
+            partner = self.env['res.partner'].browse(self.partner_id.id)
+            #fix error that triggers when partner does not have employee id
+            employee_id = partner.user_ids[0].employee_id[0].id
 
-                # workday_length = partner.user_ids[0].employee_id[0].resource_calendar_id.hours_per_day
-                # workdays = partner.user_ids[0].employee_id[0].resource_calendar_id.attendance_ids
-                # today_int = datetime.today().weekday()
+            leave_periods = self.env['hr.leave'].search([('employee_id', '=', employee_id)]).ids
 
-                # for day in workdays:
-                #     _logger.warning(f"for loop {day.dayofweek} {today_int}")
-                #     if int(day.dayofweek) == int(today_int):
-                #          _logger.warning(f"{self.event_date_start.time()} {datetime.time(hour=day.hour_from)}")
-                #         if self.event_date_start.time() >= datetime.time(hour=day.hour_from) and self.event_date_end.time() <= datetime.time(hour=day.hour_to):
+            for leave_id in leave_periods:
+                leave = self.env['hr.leave'].browse(leave_id)
+                # try:
+                if leave.date_from <= self.event_date_end and self.event_date_start <= leave.date_to:
+                    self.write({'state': 'declined'})
+                    write_state = 'declined'
+                    break
+                else:
+                    self.write({'state': 'accepted'})
+
+
+
+            if write_state != 'declined':
+                # declined_count = 0
+                # for day in workdays:        
+                #     # _logger.warning(f"for loop {day.dayofweek} {today_int}")
+                #     if int(day.dayofweek) == int(today_int): 
+                #         time_hour_from = datetime.strptime(str(int(day.hour_from)), '%H').time()
+                #         time_hour_to = datetime.strptime(str(int(day.hour_to)), '%H').time()
+                #         # _logger.warning(f" {time_hour_from} {time_hour_to}")
+
+                #         # _logger.warning(f"{time_hour_from} {time_hour_to}")
+                #         if self.event_date_start.time() >= time_hour_from and self.event_date_end.time() <= time_hour_to:
                 #             # _logger.warning('inside the last if')
-                #             rec.write({'state': 'accepted'})
+                #             self.write({'state': 'accepted'})
                 #             break
                 #         else:
-                #             rec.write({'state': 'declined'})
+                #             self.write({'state': 'declined'})
+                #             declined_count += 1
+                # if declined_count == 2:
+                #     write_state = 'declined'
 
+                # workday_length = partner.user_ids[0].employee_id[0].resource_calendar_id.hours_per_day
+                workdays = partner.user_ids[0].employee_id[0].resource_calendar_id.attendance_ids
+                today_int = datetime.today().weekday()
+                work_intervals = partner.user_ids[0].employee_id[0].resource_calendar_id[0]._work_intervals(self.event_date_start.astimezone(pytz.timezone('UTC')), 
+                                                                                                            self.event_date_end.astimezone(pytz.timezone('UTC')))
 
-            _logger.warning(f' BYPIDI WRITE {self} {vals} {res}')
+                current_tz = pytz.timezone('UTC')
+                if len(work_intervals._items) != 0:
+                    state_bool = True
+                    for count, interval in enumerate(work_intervals._items[0]):
+                        # _logger.warning(count)
+                        # _logger.warning(interval)
+                        if count == 0:
+                            if current_tz.localize(self.event_date_start) >= interval:
+                                # _logger.warning("A")
+                                continue
+                            else:
+                                state_bool = False
+                                # _logger.warning("B")
+                                # _logger.warning(current_tz.localize(self.event_date_start))
+                                # _logger.warning(interval)
+                                break
+
+                        if count == 1:
+                            if current_tz.localize(self.event_date_end) <= interval:
+                                # _logger.warning("C")
+                                continue
+                            else:
+                                # _logger.warning("D")
+                                state_bool = False
+                                break
+
+                    if not state_bool:
+                        filtered = list(filter(lambda day: int(day.dayofweek) == int(today_int), workdays))
+                        # _logger.warning(self.event_date_start.hour)
+                        # _logger.warning(filtered[0].hour_to)
+                        # _logger.warning(self.event_date_end.hour)
+                        # _logger.warning(filtered[1].hour_from)
+                        hour_to_datetime = current_tz.localize(self.event_date_start.replace(hour=int(filtered[0].hour_to)))
+                        hour_from_datetime = current_tz.localize(self.event_date_end.replace(hour=int(filtered[0].hour_from)))
+                        if current_tz.localize(self.event_date_start) <= hour_to_datetime:
+                            self.write({'state': 'tentative'})
+                            write_state = 'tentative'
+                        else:
+                            self.write({'state': 'declined'})
+                            write_state = 'declined'
+                            # _logger.warning("E")
+                else:
+                    # _logger.warning(current_tz.localize(self.event_date_start))
+                    # filtered = list(filter(lambda day: int(day.dayofweek) == int(today_int), workdays))
+
+                    self.write({'state': 'declined'})
+                    write_state = 'declined'
+                    # _logger.warning(f"F {write_state}")
+
+            attendee_ids = self.event_id.attendee_ids
+            ID = self.id            
+            if not self.env.context.get('dont_write'):
+                for attendee_id in attendee_ids:
+                    attendee_id.with_context({'dont_write': True}).write({'state': write_state})
+
+        # _logger.warning(f' BYPIDI WRITE {self} {vals} {res}')
         return res
