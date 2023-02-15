beaf9ae15f98193baea920da7cb0a6372375425f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-15 09:09:10 +0100

    Update __manifest__.py
    correcting manifest data

--- calendar_skills_allergies_glue/__manifest__.py
+++ calendar_skills_allergies_glue/__manifest__.py
@@ -1,17 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2023- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'Calendar: Skills and allergies glue module',
     'version': '14.0.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Compare contract and partner skills & allergies.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
     'category': 'Calendar',
-    'summary': 'Compare contract and partner skills & allergies',
-    'licence': 'AGPL-3',
     'description': """
-    Adds a check if sklls and allergies match for a calendar.attendee
+    Adds a check if sklls and allergies match for a calendar.attendee.
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'http://www.vertel.se',
+    'website': 'https://vertel.se/apps/odoo-calendar/calendar_skills_allergies_glue',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-calendar',
+    # Any module necessary for this one to work correctly
+
     'depends': ['calendar_attendee_planning', 'contract_attendee', 'contract_allergic', 'contract_cleaner', 'contract'],
     'data': [
         'views/calendar_view.xml',
     ],
     'auto_install': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
