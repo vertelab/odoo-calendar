f9238a6ae5570747550f6e3c854e1490465813ed
Giovanni Francesco Capalbo <giovanni@therp.nl>
2024-01-31 15:56:30 +0100

    [ADD] avoid corner case when person booking == employee connected to event

--- website_calendar_event/controllers/main.py
+++ website_calendar_event/controllers/main.py
@@ -5,6 +5,7 @@ from odoo import http, _, fields
 from odoo.http import request
 
 from odoo.addons.website_calendar_ce.controllers.main import WebsiteCalendar
+#from odoo.exceptions import AccessError
 
 
 _logger = logging.getLogger(__name__)
@@ -23,7 +24,12 @@ class ExtendedWebsiteCalendar(WebsiteCalendar):
         customer_id = None
         if related_users:
             customer_id = related_users[0][1]
-
+        # creating the task will cause error if there is no customer_id. 
+        # this will happen when the person Booking is the same as the Employee connected
+        # to the booking , in that case we set the correct customer.
+        # become the user_id of project task. so the default for customer_id is set to  
+        customer_id = [x for x in data.get('partner_ids', []) if x[1] == Employee.user_partner_id.id]
+ 
         data = {
                 'name': event.name,
                 'project_id': booking_type.related_project_id.id,
