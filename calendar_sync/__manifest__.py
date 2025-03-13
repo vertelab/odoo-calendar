196d8468de1e63e0647eb98179ff588fbdc32ada
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-03-13 15:59:32 +0100

    Update __manifest__.py

--- calendar_sync/__manifest__.py
+++ calendar_sync/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Calendar: Sync',
-    'version': '14.0.0.0.1',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Syncs the Odoo calendar with other apps.',
     'category': 'Calendar',
