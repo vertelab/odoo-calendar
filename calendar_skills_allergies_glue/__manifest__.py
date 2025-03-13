0d730c24fc1a49576ae9a5e728e450ce0c4ec6f0
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-03-13 15:59:09 +0100

    Update __manifest__.py

--- calendar_skills_allergies_glue/__manifest__.py
+++ calendar_skills_allergies_glue/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Calendar: Skills and allergies glue module',
-    'version': '14.0.0.1.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Compare contract and partner skills & allergies.',
     # Categories can be used to filter modules in modules listing
