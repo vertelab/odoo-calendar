0f95e86e989afe5105e805a2c4d14793ce678369
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-03-13 16:00:22 +0100

    Update __manifest__.py

--- ics_import_holidays/__manifest__.py
+++ ics_import_holidays/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Calendar: Import holidays',
-    'version': '14.0.0.0.1',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Import holidays will update  monthly basis so you are always on the right ',
     'category': 'Calendar',
