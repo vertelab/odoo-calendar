8d06a5db8ea53710e15b43f608c94afc9ce0856a
Anders Wallenquist <anders.wallenquist@vertel.se>
2025-06-12 09:24:08 +0000

    calendar_public_holiday_nager: dual holiday accumulate on same day

--- calendar_public_holiday_nager/models/calendar_public_holiday.py
+++ calendar_public_holiday_nager/models/calendar_public_holiday.py
@@ -1,12 +1,13 @@
 # Copyright 2025 Vertel AB
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import datetime
-import requests
-
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError,UserError
+from datetime import datetime
+import logging
+import requests
 
+_logger = logging.getLogger(__name__)
 
 class ResourceCalendarPublicHoliday(models.Model):
     _inherit = "calendar.public.holiday"
@@ -18,7 +19,23 @@ class ResourceCalendarPublicHoliday(models.Model):
             if response.status_code == 200:
                 cal.line_ids.unlink()
                 for day in response.json():
-                    cal.line_ids.create({'name': day['localName'],'date':day['date'],'public_holiday_id':cal.id})
+                    _logger.warning(f"{day=}")
+                    holidays = cal.line_ids.filtered(lambda d: d.date == datetime.strptime(day['date'], "%Y-%m-%d").date())
+                    _logger.warning(f"{day=} {holidays=}")
+                    if len(holidays) > 0: # Only one holyday at the same date
+                        holidays[0].name += f", {day['localName']}"
+                    else:
+                        cal.line_ids.create({'name': day['localName'],'date':day['date'],'public_holiday_id':cal.id})
+# TODO lägga på name  och localName via översättning
+                        # ~ self.env['ir.translation'].create({
+                            # ~ 'name': f"{holiday._name},name",
+                            # ~ 'res_id': holiday.id,
+                            # ~ 'lang': 'sv_SE',  # Byt till önskat språk
+                            # ~ 'type': 'model',
+                            # ~ 'value': day['translatedName'],
+                            # ~ 'state': 'translated',
+                        # ~ })
+
             else:
                 raise UserError(f"Could not fetch holidays {response.status_code} - {response.reason}{response.text}\n{url}")
 
