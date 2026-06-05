fece7532ec1d22867bec545831adf0bbc632688d
Anders Wallenquist <anders.wallenquist@vertel.se>
2026-06-05 08:55:09 +0000

    calendar_caldav: hook issue

--- calendar_caldav/hooks.py
+++ calendar_caldav/hooks.py
@@ -1,4 +1,4 @@
-def post_init_hook(cr, registry):
-    cr.execute(
+def post_init_hook(env):
+    env.cr.execute(
         "UPDATE calendar_event SET uuid = gen_random_uuid()::text WHERE uuid IS NULL"
     )
